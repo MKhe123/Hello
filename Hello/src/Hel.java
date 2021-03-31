7c86052e049ef33f077ffe8ff47a12cb0f6cd3b2
KK <k349211108@163.com>
2021-03-31 18:00:11 +0800

    第一次使用idea提交文件到本地仓库

---
+++ Hello/src/Hel.java
@@ -0,0 +1,10 @@
+/**
+ * @Auther: Koreleone
+ * @Date: 2021-03-31 17:55
+ * @Description:
+ */
+public class Hel {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
